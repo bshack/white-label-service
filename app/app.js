524c3ccb21ff752b40ec23248fa725bfef8b51e1
William Shackelford <williamshackelford@Williams-MacBook-Pro.local>
2016-09-07 22:26:32 -0400

    sub some routes

--- app/app.js
+++ app/app.js
@@ -1,44 +1,149 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const helmet = require('helmet');
+const compression = require('compression')
 
+app.use(bodyParser.json());
+app.use(compression());
 app.use(helmet());
 
-app.post('/user', function(req, res) {
-    res.setHeader('Content-Type', 'application/json');
-    res.send({
-        id: '12'
-    });
+var knex = require('knex')({
+    client: 'mysql',
+    connection: {
+        host: '127.0.0.1',
+        user: 'root',
+
+        database: 'white-label-model'
+    }
 });
 
-app.get('/user/:userId', function(req, res) {
+app.post('/user', (req, res) => {
+
     res.setHeader('Content-Type', 'application/json');
-    res.send({
-        id: req.params.userId
-    });
+
+    knex('user')
+        .insert(req.body)
+        .catch(function(error) {
+            res.send(error);
+        })
+        .then((data) => {
+            res.send(data);
+        });
+
 });
 
-app.put('/user/:userId', function(req, res) {
+app.get('/user/:id', (req, res) => {
+
     res.setHeader('Content-Type', 'application/json');
-    res.send({
-        id: req.params.userId
-    });
+
+    knex
+        .select('*')
+        .from('user')
+        .where({
+            id: req.params.id
+        })
+        .catch(function(error) {
+            res.send({
+                status: false,
+                data: error
+            });
+        })
+        .then((data) => {
+            res.send({
+                status: true,
+                data: data
+            });
+        });
+
 });
 
-app.patch('/user/:userId', function(req, res) {
+app.put('/user/:id', (req, res) => {
+
     res.setHeader('Content-Type', 'application/json');
-    res.send({
-        id: req.params.userId
-    });
+
+    knex('user')
+        .where('id', req.params.id)
+        .update({
+            email: req.body.email
+        })
+        .catch(function(error) {
+            res.send({
+                status: false,
+                data: error
+            });
+        })
+        .then((data) => {
+            if (data === 1) {
+                res.send({
+                    status: true,
+                    data: data
+                });
+            } else {
+                res.send({
+                    status: false,
+                    data: data
+                });
+            }
+        });
+
 });
 
-app.delete('/user/:userId', function(req, res) {
+app.patch('/user/:id', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
-    res.send({
-        id: req.params.userId
-    });
+    knex('user')
+        .where('id', req.params.id)
+        .update({
+            email: req.body.email
+        })
+        .catch(function(error) {
+            res.send({
+                status: false,
+                data: error
+            });
+        })
+        .then((data) => {
+            if (data === 1) {
+                res.send({
+                    status: true,
+                    data: data
+                });
+            } else {
+                res.send({
+                    status: false,
+                    data: data
+                });
+            }
+        });
 });
 
-app.listen(3000, function() {
+app.delete('/user/:id', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    knex('user')
+        .where('id', req.params.id)
+        .del()
+        .catch(function(error) {
+            res.send({
+                status: false,
+                data: error
+            });
+        })
+        .then((data) => {
+            if (data === 1) {
+                res.send({
+                    status: true,
+                    data: data
+                });
+            } else {
+                res.send({
+                    status: false,
+                    data: data
+                });
+            }
+        });
+
+});
+
+app.listen(3000, () => {
     console.log('Example app listening on port 3000!');
 });
